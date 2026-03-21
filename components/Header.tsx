6a666ebfff89044f4c45a165a981726bdeb9f61b
Jonathan Santos <jonathan.amado@outlook.com>
2026-03-20 22:58:19 -0300

    Fix: ajuste header

--- components/Header.tsx
+++ components/Header.tsx
@@ -218,7 +218,7 @@ export function Header() {
                   alt="Logo Nucleo Base"
                   width={120} 
                   height={65} 
-                  className="w-[140px] h-auto lg:w-[170px] lg:h-auto object-contain" 
+                  className="w-[125px] h-auto lg:w-[155px] lg:h-auto object-contain" 
                 />
               </a>
           </div>
