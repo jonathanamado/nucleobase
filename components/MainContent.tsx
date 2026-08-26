608cdd863e6be745430551b5256c2950247c7905
Jonathan Santos <jonathan.amado@outlook.com>
2026-08-25 21:42:41 -0300

    fix: correção seguranca e acessos

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -108,7 +108,7 @@ export function MainContent() {
             <div className="relative bg-white border border-gray-100 p-7 rounded-[2.5rem] shadow-xl shadow-blue-900/5">
               <div className="mb-6">
                 <p className="text-gray-700 text-base leading-snug font-medium text-center">
-                  Você está no APP em que números brutos tornam-se <span className="text-gray-900 font-bold not-italic">decisões inteligentes e práticas</span> em sua rotina. Clique na <span className="text-blue-600 font-bold">Logo</span> <LayoutDashboard size={14} strokeWidth={3} className="inline-block text-blue-600 mb-1" />, acesse seu <span className="text-gray-900 font-bold not-italic">Painel</span> e profissionalize processos de maneira simples.
+                  Você está no APP em que números brutos tornam-se <span className="text-gray-900 font-bold not-italic">decisões inteligentes e práticas</span> em sua rotina. Clique na <span className="text-blue-600 font-bold">Logo</span> <LayoutDashboard size={14} strokeWidth={3} className="inline-block text-blue-600 mb-1" />, acesse seu <span className="text-gray-900 font-bold not-italic">Painel</span> e profissionalize seus processos.
                 </p>
               </div>
 
